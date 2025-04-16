fdfb3da1b33bf0ac5ebb95cb1053787ef07f0dc7
JulianLeQuack <julian.futurepilot@gmail.com>
2025-04-16 14:24:57 +0200

    initialized demo module with demo data

---
+++ demo/demo.py
@@ -0,0 +1,15 @@
+# Footprint Creation Imports
+from source.transformation_horizontal.create_footprints.create_CityGML_footprint import create_CityGML_footprint
+from source.transformation_horizontal.create_footprints.create_IFC_footprint_polygon import create_IFC_footprint_polygon
+from source.transformation_horizontal.create_footprints.create_DXF_footprint_polygon import create_DXF_footprint_polygon
+
+# Horizontal Registration Imports
+from source.transformation_horizontal.rigid_transformation import Rigid_Transformation
+from source.transformation_horizontal.estimate_rigid_transformation import estimate_rigid_transformation, refine_rigid_transformation
+from source.transformation_horizontal.detect_features import detect_features, filter_features_by_feature_triangle_area
+
+
+# Input Data Paths
+ifc_path = "./demo/data/3.002 01-05-0501_EG.ifc"
+create_IFC_footprint_polygon(ifc_path=ifc_path, ifc_type="IfcSlab")
+
