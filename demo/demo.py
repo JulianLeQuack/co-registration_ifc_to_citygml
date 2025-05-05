7f2ae3b5e3e8bc2333f274d782e8fb5fa7ff5043
JulianLeQuack <julian.futurepilot@gmail.com>
2025-05-05 16:17:21 +0200

    added som emore demo options

--- demo/demo.py
+++ demo/demo.py
@@ -13,15 +13,18 @@ from source.transformation_vertical.extract_elevation_labels import extract_elev
 
 
 # Input Data Paths
-ifc_path = "./demo/data/3.002 01-05-0501_EG.ifc"
+# ifc_path = "./demo/data/3.002 01-05-0501_EG.ifc"
+ifc_path_3d= "./test_data/ifc/3D_01_05_0507.ifc"
+ifc_path = "./test_data/ifc/3.003 01-05-0507_EG.ifc"
 dxf_path = "./demo/data/01-05-0501_EG.dxf"
 dxf_path_point_symmetric = "./demo/data/01-05-0507_EG.1.dxf"
-citygml_path = "./demo/data/DEBY_LOD2_4959457.gml"
+citygml_path = "./test_data/citygml/TUM_LoD2_Full_withSurrounds.gml"
 data_path = "./demo/data/"
 
 # Model footprint settings
-citygml_building_ids = ["DEBY_LOD2_4959457"]
-ifc_type = "IfcSlab"
+# citygml_building_ids = ["DEBY_LOD2_4959457"]
+citygml_building_ids = ['DEBY_LOD2_4959793', 'DEBY_LOD2_4959323', 'DEBY_LOD2_4959321', 'DEBY_LOD2_4959324', 'DEBY_LOD2_4959459', 'DEBY_LOD2_4959322', 'DEBY_LOD2_4959458']
+ifc_type = "IfcWall"
 dxf_layer = "A_09_TRAGDECKE"
 
 # Create Footprints
@@ -38,9 +41,10 @@ citygml_features = detect_features(footprint=citygml_footprint, angle_threshold_
 
 # Filter features by eliminating featrues that span up small triangles with adjacent features to only keep significant features
 print("Filtering Features...")
-ifc_features_filtered = filter_features_by_feature_triangle_area(features=ifc_features, min_area=15)
-dxf_features_filtered = filter_features_by_feature_triangle_area(features=dxf_features, min_area=15)
-citygml_features_filtered = filter_features_by_feature_triangle_area(features=citygml_features, min_area=15)
+ifc_features_filtered = filter_features_by_feature_triangle_area(features=ifc_features, min_area=5)
+dxf_features_filtered = filter_features_by_feature_triangle_area(features=dxf_features, min_area=5)
+citygml_features_filtered = filter_features_by_feature_triangle_area(features=citygml_features, min_area=5)
+print(f"Filtered Features: IFC: {len(ifc_features_filtered)}, DXF: {len(dxf_features_filtered)}, CityGML: {len(citygml_features_filtered)}")
 
 # Estimate Rigid Transformation for IFC to CityGML
 print("Estimating Rigid Transformation for IFC Footprint...")
@@ -77,4 +81,4 @@ transformed_elevation_labels_dxf = refined_transformation_dxf_to_ifc.transform_e
 
 # Apply the transformations to the DXF and IFC files
 print("Applying Transformations to DXF and IFC files...")
-transformed_ifc_file = refined_transformation_ifc_to_citygml.transform_ifc(input_ifc_path=ifc_path, output_ifc_path=data_path + "transformed_ifc.ifc")
+transformed_ifc_file = refined_transformation_ifc_to_citygml.transform_ifc(input_ifc_path=ifc_path_3d, output_ifc_path=data_path + "transformed_ifc.ifc", z=515.83-5.35)
